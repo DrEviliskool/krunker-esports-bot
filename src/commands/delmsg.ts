e515ec043043ad65dea33860024ad4834445c2b0
DrEvilisKool <drevil6766@gmail.com>
2023-04-23 01:21:21 +0200

    Updated cmds

--- src/commands/delmsg.ts
+++ src/commands/delmsg.ts
@@ -1,4 +1,4 @@
-import { CategoryChannel, ChannelType, Client, Message } from "discord.js";
+import { Client, Message, TextChannel } from "discord.js";
 import { OWNERS } from "../config";
 
 export const delmsg = async (msg: Message, args: string[], client: Client) => {
@@ -6,7 +6,34 @@ export const delmsg = async (msg: Message, args: string[], client: Client) => {
     if (!OWNERS.some(ID => msg.author?.id.includes(ID))) {
         return
     }
-    const messageId = args[0]
+    let messageId
+    let channel = msg.mentions.channels?.first() as TextChannel || await client.channels.cache.get(args[0]) as TextChannel
+    if (!channel) {
+        channel = msg.channel as TextChannel
+        messageId = args[0]
+    }
 
-   msg.channel.messages.fetch(messageId).then(ok => ok.delete())
+    if(!messageId) return msg.react("❌").then(ok => {
+        setTimeout(() => {
+            msg.delete()
+        }, 1000 * 5);
+    })
+
+    const realmsg = await channel.messages.fetch(messageId).catch(err => { msg.react("❌").then(ok => { 
+        setTimeout(() => {
+            msg.delete()
+        }, 1000 * 5);
+    }) } ) as Message
+
+
+    if(realmsg) {
+        realmsg.delete().then(e => {
+            msg.react("✅").then(ok => {
+
+                setTimeout(() => {
+                    msg.delete()
+                }, 1000 * 5);
+            })
+        })
+    }
 }
